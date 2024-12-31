cec8358918262c0f1de3a392786ce0ff1b941835
MohamedRajwan <rajwanwebs@gmail.com>
2024-12-31 12:58:12 +0530

    Update website with new changes

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,36 +1,33 @@
 import React from 'react';
-import ResumeImg from '../assets/resume.jpg'; // This is an image preview of your resume, optional
+import ResumeImg from '../assets/resume.jpg';
 
 export default function Resume() {
   const config = {
-    link: '/resume.pdf', // This points to your PDF file in the public folder
+    link: '/resume.pdf',  // Link to the PDF file
   };
 
   return (
-    <section id="resume" className="flex flex-col md:flex-row bg-secondary px-5">
-      {/* Image Section (optional) */}
+    <section id="resume" className="flex flex-col md:flex-row bg-secondary px-5 py-8">
       <div className="py-5 md:w-1/2 flex justify-center md:justify-end">
-        <img className="w-[300px]" src={ResumeImg} alt="Resume Thumbnail" />
+        <img className="w-[250px] md:w-[300px] rounded-lg" src={ResumeImg} alt="Resume Thumbnail" />
       </div>
-
-      {/* Resume Information Section */}
       <div className="md:w-1/2 flex justify-center">
         <div className="flex flex-col justify-center text-black">
-          <h1 className="text-4xl text-gray-300 border-b-4 border-primary mb-5 w-[140px] font-bold">Resume</h1>
-          
-          {/* Link to Download or View Resume */}
-          <p className="pb-5 text-gray-300">
-            You can view or download my resume 
-            <a
-              className="btn"
-              href={config.link}   // This links to your PDF file
-              target="_blank"      // Opens the PDF in a new tab
-              rel="noopener noreferrer"
-              download              // Allows users to download the file directly
-            >
-              {' '} Download
-            </a>
+          <h1 className="text-3xl md:text-4xl text-gray-300 border-b-4 md:border-b-4 border-primary mb-5 w-[120px] md:w-[140px] font-bold text-left">
+            Resume
+          </h1>
+          <p className="pb-5 text-gray-300 text-left">
+            You can download my resume
           </p>
+          <div className="text-left">
+            <a
+              className="btn mt-3 py-2 px-4 bg-primary text-white rounded-md hover:bg-primary-dark"
+              href={config.link}
+              download
+            >
+              Download
+            </a>
+          </div>
         </div>
       </div>
     </section>
